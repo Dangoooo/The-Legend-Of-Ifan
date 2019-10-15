92cde42a699fa4f34535ea80cc19f169d4a27150
Dangoooo <912017212@qq.com>
2019-10-15 23:21:56 +0800

    Update

---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Items")]
+public class InventoryItem : ScriptableObject
+{
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemSprite;
+    public int numberOfItem;
+    public bool usable;
+    public bool unique; 
+}
